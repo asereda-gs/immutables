b0dae1456e499ab482aa4417a621d002acaa11db
Andrei Sereda <25229979+asereda-gs@users.noreply.github.com>
2019-02-10 22:52:05 -0500

    trying more generic operators

---
+++ criteria/src/org/immutables/criteria/constraints/Operator.java
@@ -0,0 +1,5 @@
+package org.immutables.criteria.constraints;
+
+public interface Operator {
+
+}
---
+++ criteria/src/org/immutables/criteria/constraints/Operators.java
@@ -0,0 +1,9 @@
+package org.immutables.criteria.constraints;
+
+public enum Operators implements Operator {
+
+
+  EQ,
+  NE;
+
+}
